130c9972dcde81aa8a24f2b5de575b45263ebbdd
Dipankur Saikia <et22bthcs039@kazirangauniversity.in>
2025-07-31 18:36:57 +0530

    changed

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -29,7 +29,7 @@ function Login() {
                   type="email"
                   name="email"
                   placeholder="Enter email..."
-                  className="flex-grow bg-transparent outline-none text-gray-700 placeholder-gray-400 w-full"
+                  className="flex-grow bg-transparent outline-none text-gray-700 placeholder-gray-400"
                   id="emailId"
                 />
               </div>
@@ -48,7 +48,7 @@ function Login() {
                   type="password"
                   name="password"
                   placeholder="Enter password..."
-                  className="flex-grow bg-transparent outline-none text-gray-700 placeholder-gray-400 w-full"
+                  className="flex-grow bg-transparent outline-none text-gray-700 placeholder-gray-400"
                   id="passwordId"
                 />
               </div>
