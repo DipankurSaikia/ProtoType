28fe6eb16ca2d5821cb1fa5e5ccdb95f37753e67
Dipankur Saikia <et22bthcs039@kazirangauniversity.in>
2025-07-31 18:15:54 +0530

    login input

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -29,7 +29,7 @@ function Login() {
                   type="email"
                   name="email"
                   placeholder="Enter email..."
-                  className="flex-grow bg-transparent outline-none text-gray-700 placeholder-gray-400"
+                  className="flex-grow bg-transparent outline-none text-gray-700 placeholder-gray-400 max-w-full"
                   id="emailId"
                 />
               </div>
